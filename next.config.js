6757e934647a12f54ecc1147ed085670345873f7
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2024-03-11 21:00:58 -0400

    New article

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withNextra = require('nextra')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'cdn.hashnode.com'],
   },
 };
 
